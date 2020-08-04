e958b7e20360bf17ec5d08bec893742dd6a4efc1
Wilthon Peña <wilthon-pena95@hotmail.com>
2020-08-03 22:45:25 -0500

    Ajuste de contenido y recaptcha

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -6,7 +6,7 @@ if(empty($_POST['name'])      ||
    empty($_POST['message'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-   echo "No arguments Provided!";
+   echo "No se proporcionaron argumentos!";
    return false;
    }
    
