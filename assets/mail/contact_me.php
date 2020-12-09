c9a741897cf8cbb84c49ae5f7a3a08b8572d18d7
Wilthon Peña <wilthon-pena95@hotmail.com>
2020-12-09 11:42:02 -0500

    Ajuste de redes sociales y envío de email

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -1,4 +1,15 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require '../../vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+$mail->isSMTP();
+
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
@@ -14,13 +25,39 @@ $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-   
-// Create the email and send the message
-$to = 'wilthon-pena95@hotmail.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Software Solution:  $name";
-$email_body = "Recibió un nuevo mensaje del formulario de contacto de su sitio web.\n\n"."Aquí están los detalles:\n\nNombres: $name\n\nEmail: $email_address\n\nTeléfono: $phone\n\nMensaje:\n$message";
-$headers = "From: wilthon-pena95@hotmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
+
+try {
+    //Server settings
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'softwipe.info@gmail.com';                     // SMTP username
+    $mail->Password   = 'Softwipe1995';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('wilthon-pena95@hotmail.com', 'Softwipe');
+    $mail->addAddress('wilthon-pena95@hotmail.com', 'Wilthon Peña');     // Add a recipient
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    // Attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Nuevo contacto Softwipe';
+    $mail->Body    = "Recibió un nuevo mensaje del formulario de contacto de su sitio web.<br><br>"."Aquí están los detalles:<br><br>Nombres: $name<br>Email: $email_address<br>Teléfono: $phone<br>Mensaje:\n$message <br><br>";
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+	//Activo condificacción utf-8
+	$mail->CharSet = 'UTF-8';
+    $mail->send();
+    return true;
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+	return false;
+}
 ?>
